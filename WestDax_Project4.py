da909de5c006e2858340f08c5b9c17ba71e2357d
DaxWest <awest49@uwo.ca>
2024-12-06 13:17:49 -0500

    working on the structure of the functions

--- WestDax_Project4.py
+++ WestDax_Project4.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 #setting constants
 h_bar = 1
 m = 1/2
+t_init = 0
 
 def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=[10, 0, 0.5]):
     '''
@@ -18,8 +19,17 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=
     :param wparam: list of parameters for initial conditions in the form [sigma0, x0, k0]
     :return: two 1D arrays giving the x and t grid values (in the form of a 2D array)
     '''
+    sigma0, x0, k0 = wparam[0], wparam[1], wparam[2]
 
-def sch_plot(x,t, output=psi):
+    #intitializing these for now, they will contain more information later
+    phi_x = 1
+    phi_t = 1
+    prob = 1
+
+    eqn_sol = np.array([[phi_x], [phi_t], [prob]])
+    return eqn_sol
+
+def sch_plot(x,t, output='psi'):
     '''
 
     :param x:
@@ -27,3 +37,10 @@ def sch_plot(x,t, output=psi):
     :param output: takes a str ('psi' or 'prob') which designates the output of the function
     :return: psi (plot of the real part of the schrodinger function) or prob (plot of the particle probability density)
     '''
+    if output == 'psi':
+        #this is the real portion
+
+    elif output == 'prob':
+        #probablity density
+
+
