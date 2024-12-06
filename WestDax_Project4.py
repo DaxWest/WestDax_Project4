d6b4bbedbee425b27dff5048fd8933bca84ab3b3
DaxWest <awest49@uwo.ca>
2024-12-06 13:34:49 -0500

    added the option to save the figure in the plotting function

--- WestDax_Project4.py
+++ WestDax_Project4.py
@@ -17,7 +17,7 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=
     :param length: size of spatial grid. Default is 200 (-100 to +100)
     :param potential: a 1D array of the spatial index values for which the potential must be V(x)=1
     :param wparam: list of parameters for initial conditions in the form [sigma0, x0, k0]
-    :return: two 1D arrays giving the x and t grid values (in the form of a 2D array)
+    :return: a 2D array containing phi_x, phi_t and, prob which are all 1D arrays
     '''
     sigma0, x0, k0 = wparam[0], wparam[1], wparam[2]
 
@@ -29,18 +29,30 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=
     eqn_sol = np.array([[phi_x], [phi_t], [prob]])
     return eqn_sol
 
-def sch_plot(x,t, output='psi'):
+def sch_plot(x,t, P, output='psi', save=False):
     '''
 
-    :param x:
-    :param t:
+    :param x: phi_x output from sch_eqn (a 1D array)
+    :param t: phi_t output from sch_eqn (a 1D array)
+    :param P: prob output from sch_eqn (a 1D array)
     :param output: takes a str ('psi' or 'prob') which designates the output of the function
+    :param save: allows the user to choose to save the figure when set as True. Default False.
     :return: psi (plot of the real part of the schrodinger function) or prob (plot of the particle probability density)
     '''
+
+    x_pos = x
+    t_val = t
+    prob_density = P
+
     if output == 'psi':
         #this is the real portion
+        if save == True:
+            figure = plt.savefig()
 
     elif output == 'prob':
         #probablity density
+        if save == True:
+            figure = plt.savefig()
 
+    return figure
 
