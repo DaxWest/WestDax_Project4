e0830ac026332bd02e24b3e0b4a77638eb3f26c6
DaxWest <awest49@uwo.ca>
2024-12-06 12:27:21 -0500

    Test commit and the initialization of the main function

---
+++ WestDax_Project4.py
@@ -0,0 +1,14 @@
+
+
+def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=[10, 0, 0.5]):
+    '''
+
+    :param nspace: number of spatial grid points
+    :param ntime: number of time steps to be evolved
+    :param tau: time step
+    :param method: Default 'ftcs', takes strings: 'ftcs' or 'crank'
+    :param length: size of spatial grid. Default is 200 (-100 to +100)
+    :param potential: a 1D array of the spatial index values for which the potential must be V(x)=1
+    :param wparam: list of parameters for initial conditions in the form [sigma0, x0, k0]
+    :return:
+    '''
